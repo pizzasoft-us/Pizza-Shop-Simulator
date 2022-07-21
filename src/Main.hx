a5f636b353c16c85f98a451a7b761ca553161d04
BasicCorruption <saytleader@outlook.com>
2022-07-21 10:09:36 -0400

    move fps counter

--- src/Main.hx
+++ src/Main.hx
@@ -15,7 +15,7 @@ class Main extends Sprite
 		super();
 		SessionStorage.initJSONStorage();
 		Lib.current.addChild(new FlxGame(0, 0, MenuState, 1, 60, 60, true, false));
-		Lib.current.addChild(new FPS(10, 10, 0x000000));
+		Lib.current.addChild(new FPS(10, 20, 0x000000));
 		#if html5
 		FlxG.sound.playMusic(Resources.BusyCity__mp3);
 		#else
